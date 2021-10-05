d22035f1280b6b64c7009f727ff1d12172744ae7
Daniel M. Matongo <m_matongo@icloud.com>
2021-10-05 11:18:22 +0200

    update app.js and its styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import logo from './assets/rocket.png';
 import Nav from './components/Nav';
 import './App.css';
+import Rockets from './components/rockets/Rockets';
+import pullRockets from './redux/slices/rocketSlice';
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(async () => {
+    dispatch(pullRockets());
+  });
+
   return (
     <div className="App">
       <Router>
         <header className="main-header">
           <img src={logo} className="logo" alt="logo" />
+          <Nav />
         </header>
-        <Nav />
         <Switch>
-          <Route exact path="/" />
+          <Route exact path="/">
+            <Rockets />
+          </Route>
           <Route path="/missions" />
           <Route path="/profile" />
         </Switch>
       </Router>
     </div>
   );
-}
+};
 
 export default App;
