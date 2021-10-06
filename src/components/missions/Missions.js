66a67e88845d4ac98baf9624781aeeff9dbecd44
themonster2015 <quynhyen.vothi@gmail.com>
2021-10-06 11:50:50 +0200

    added join functionality for missions, added joined mission listing to profile page

--- src/components/missions/Missions.js
+++ src/components/missions/Missions.js
@@ -1,30 +1,50 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { displayMissions, joinMission } from '../../redux/missions/missions';
 
 export default function Missions() {
   const missions = useSelector((state) => state.missions);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(displayMissions());
+  }, []);
+
+  const handleClick = (id) => {
+    console.log(id);
+    dispatch(joinMission(id));
+  };
 
   const parsed = missions.map((mission) => (
-    <tr key={mission.id}>
+    <tr key={mission.mission_id}>
       <td className="mName">{mission.mission_name}</td>
       <td className="mDescription">{mission.description}</td>
-      <td className="mStatusRow">Place Holder</td>
-      <td className="mBook"><button type="button" className="btn-join-mission">Join Mission</button></td>
+      <td className="mStatusRow">
+        <span className={mission.reserved ? 'Active' : 'inActive'}>
+          {mission.reserved ? 'Active Member' : 'Not a Member'}
+          {' '}
+        </span>
+      </td>
+      <td className="mBook"><button type="button" onClick={() => handleClick(mission.mission_id)} className="btn-join-mission">Join Mission</button></td>
     </tr>
   ));
 
   return (
     <div className="container">
       <table>
-        <tr>
-          <th className="mTitle">Mission</th>
-          <th className="mDes">Description</th>
-          <th className="mStatus">Status</th>
-          <th>
-            {' '}
-          </th>
-        </tr>
-        {parsed}
+        <thead>
+          <tr>
+            <th className="mTitle">Mission</th>
+            <th className="mDes">Description</th>
+            <th className="mStatus">Status</th>
+            <th>
+              {' '}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {parsed}
+        </tbody>
       </table>
     </div>
   );
--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -3,6 +3,7 @@ const initialState = [];
 const api = 'https://api.spacexdata.com/v3/missions';
 
 const DISPLAY_MISSIONS = 'missionStore/missions/DISPLAY_MISSIONS';
+const JOIN_MISSION = 'missionStore/missions/JOIN_MISSION';
 
 export const displayMissions = () => (dispatch) => {
   fetch(api)
@@ -24,10 +25,23 @@ export const displayMissions = () => (dispatch) => {
     });
 };
 
+export const joinMission = (payload) => ({
+  type: JOIN_MISSION,
+  payload,
+});
+
 const missionReducer = (state = initialState, action) => {
   switch (action.type) {
     case DISPLAY_MISSIONS:
-      return [...state, ...action.payload];
+      return [...action.payload];
+    case JOIN_MISSION:
+      return state.map((mission) => {
+        if (mission.mission_id === action.payload) {
+          return { ...mission, reserved: !mission.reserved };
+        }
+        return mission;
+      });
+
     default:
       return state;
   }
